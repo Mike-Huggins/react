36536222029f65aa77dea8112e76997ff93d4699
Mike H <mikeyh1001@hotmail.com>
2019-04-17 13:43:12 +0100

    receiving forecast summary in the data files and outputting

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
-import ForecastSummaries from './forecast-summary';
+import ForecastSummaries from './forecast-summaries';
 
 const App = props => (
   <div className="forecast"><LocationDetails city={props.location.city} country={props.location.country} />
