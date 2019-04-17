09f0b89d289faa42a6e1bba271151b0afe7f9f04
Mike H <mikeyh1001@hotmail.com>
2019-04-17 13:22:47 +0100

    created 2 file types as originally intended

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
+import ForecastSummaries from './forecast-summary';
 
-const App = props => <LocationDetails city={props.location.city} country={props.location.country} />;
+const App = props => (
+  <div className="forecast"><LocationDetails city={props.location.city} country={props.location.country} />
+    <ForecastSummaries forecasts={props.forecasts} />
+  </div>
+);
 
 App.propTypes = {
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+  forecasts: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const ForecastSummary = props => (<h1 className="forecast-summary"><div className="date"><span>{props.date}</span></div>,
-  <div className="temperature"><span>{props.temperature}</span></div>,
-  <div className="description"><span>{props.description}</span></div> ,
-  <div className="icon"><span>{props.icon}</span></div>,
-</h1> 
+const ForecastSummary = props => (
+  <div>
+    <span className="date">{props.date}</span>
+    <span className="temperature">{props.temperature}</span>
+    <span className="description">{props.description}</span>
+    <span className="icon">{props.icon}</span>
+  </div>
 );
 
 ForecastSummary.propTypes = {
@@ -17,33 +18,3 @@ ForecastSummary.propTypes = {
 };
 
 export default ForecastSummary;
-
-/*const LocationDetails = props => <h1 className="location-details">{props.city}, {props.country}</h1>;
-
-LocationDetails.propTypes = {
-  city: PropTypes.string.isRequired,
-  country: PropTypes.string.isRequired,
-};
-
-export default LocationDetails;
-
-it('renders the date', () => {
-    const wrapper = shallow((
-      <ForecastSummary
-        date="mockDate"
-        temperature="mockTemperature"
-        description="mockDescription"
-        icon="mockIcon"
-      />
-    ));
-    expect(wrapper.find('.forecast-summary').text()).toEqual('mockDate');
-
-    it('renders city and country in h1 element', () => {
-        const wrapper = Enzyme.shallow((
-          <LocationDetails city="foo" country="bar" />
-        ));
-        const text = wrapper.find('h1').text(); 
-        expect(text).toEqual('foo, bar');
-      });
-
-*/
