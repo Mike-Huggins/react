1d8b507fd478545e888ebee4bc38841e23c21b0e
Mike H <mikeyh1001@hotmail.com>
2019-04-21 09:18:04 +0100

    value of text read and put into this.state#

--- src/components/searchform.jsx
+++ src/components/searchform.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-const SearchForm = props => (
-  <div>
-    <input type="text" />
-    <button>Search </button>
-  </div>
-);
+class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      searchText: event.target.value,
+    });
+    return console.log(this.state.searchText);
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleOnChange} type="text" value={this.state.searchText} />
+        <button type="submit"> Search </button>
+      </div>
+    );
+  }
+}
 
 export default SearchForm;
